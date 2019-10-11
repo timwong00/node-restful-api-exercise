c8bb4da58365583e101a521e5e0b400c7770d7ca
Tim Wong <wong.hsw@gmail.com>
2019-10-11 15:19:27 -0400

    basic routes setup

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,4 +2,41 @@ const express = require('express');
 // handle different routes/endpoints
 const router = express.Router();
 
-router.get('/')
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET request to /products'
+    })
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST request to /products'
+    })
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    })
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    })
+});
+module.exports = router;
--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
 // 'use' sets up a middleware
 // /products will forward to products.js
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
