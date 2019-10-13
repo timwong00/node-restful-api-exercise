3b52308c97019cebd9c32a4dfb2d2602e601f4ec
Tim Wong <wong.hsw@gmail.com>
2019-10-13 13:27:06 -0400

    added more routes + nodemon + morgan

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 // 'use' sets up a middleware
+// Routes which should handle requests
 // /products will forward to products.js
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
