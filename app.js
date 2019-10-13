9e8b7691f457e46ba7b62168df45b35e5f3cb93e
Tim Wong <wong.hsw@gmail.com>
2019-10-13 16:30:25 -0400

    added body parser and able to handle CORS error

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 // 'use' sets up a middleware
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// append headers
+// prevent CORS errors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // * = access to any origin or restrict to a 'http://'
+    res.header(
+    'Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    ); // * or Origin, X-Requested-With, Content-Type, Accept, Authorization
+    if (req.method === 'OPTIONS') { // check if incoming req.method (method property which gives access to HTTP method
+        // BROWSER will always send a options request first when post or put request is sent
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({}); // sends back response adding the headers request will continue
+    }
+    next(); // other routes can take over
+})  
 
 // Routes which should handle requests
 // /products will forward to products.js
