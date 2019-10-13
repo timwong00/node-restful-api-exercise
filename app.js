36dcf56045f9d10ff07eff1e286f3cb1f704d8c0
Tim Wong <wong.hsw@gmail.com>
2019-10-13 13:36:58 -0400

    added error handling

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 // 'use' sets up a middleware
+app.use(morgan('dev'));
+
 // Routes which should handle requests
 // /products will forward to products.js
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error); // forwards the request i.e. error request
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+})
 
 module.exports = app;
