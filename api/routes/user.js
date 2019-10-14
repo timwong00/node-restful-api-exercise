fca0201426a92b07f0d4f59909735539679a4617
Tim Wong <wong.hsw@gmail.com>
2019-10-14 14:55:20 -0400

    added user login and jwt signing

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -45,6 +46,48 @@ router.post("/signup", (req, res, next) => {
           }
         });
       }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({ message: "Auth failed" });
+        //   .status(404)
+        //   .json({ message: "Email not found, user doesn't exist" }); //attackers can use this to check if users exists or not
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({ message: "Auth failed" });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h"
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed"
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err),
+        res.status(500).json({
+          error: err
+        });
     });
 });
 
